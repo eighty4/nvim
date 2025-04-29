0acf0ad4c8e937cc7c5b1b26bbd7514948b005bf
Adam McKee <adam.be.g84d@gmail.com>
2025-04-29 09:58:53 -0500

    add testnav

---
+++ lua/plugins/testnav.lua
@@ -0,0 +1,11 @@
+return {
+    "eighty4/testnav.vim",
+    --dir = "/Users/adam/work/eighty4/testnav.vim",
+    cmd = {
+        "TestNav",
+    },
+    keys = {
+        { "<leader>tn", "<cmd>TestNav<cr>", desc = "Nav between source and test" },
+    },
+}
+
