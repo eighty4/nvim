266ba225016946adc3aafc2bc203f340b3e4e9a5
Adam McKee <adam.be.g84d@gmail.com>
2025-04-30 13:08:18 -0500

    lazy update freq 1hr > 48hrs

--- lua/adam/lazy.lua
+++ lua/adam/lazy.lua
@@ -21,16 +21,14 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Setup lazy.nvim
+-- https://lazy.folke.io/configuration
 require("lazy").setup({
   spec = {
-    -- import your plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    frequency = 172800, -- 48 hours
+  },
 })
 
