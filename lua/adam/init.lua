9dc11c228ca985fe38914900ff4bee83e1e187be
Adam McKee <adam.be.g84d@gmail.com>
2025-05-08 00:23:13 -0500

    reorg key mappings

--- lua/adam/init.lua
+++ lua/adam/init.lua
@@ -27,12 +27,10 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 2
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
-vim.g.mapleader = " "
--- lua/adam/remap.lua
+++ lua/adam/remap.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- map F1 to noop to prevent :help shortcut
+vim.keymap.set("n", "<F1>", "<Nop>")
