725fc6530b7c884d72d03a08d5ed8dcc84137c71
Adam McKee <adam.be.g84d@gmail.com>
2025-05-20 10:48:58 -0500

    switch to kanagawa dragon

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -3,7 +3,7 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd([[colorscheme kanagawa]])
+      vim.cmd([[colorscheme kanagawa-dragon]])
       -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
